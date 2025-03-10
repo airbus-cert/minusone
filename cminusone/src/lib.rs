ee666c0592a71ce081610f978fb7d74837d0dbf9
atxr <alexandre.tullot@protonmail.com>
2025-03-10 17:13:48 +0100

    YAT

--- cminusone/src/lib.rs
+++ cminusone/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{ffi::c_int, mem, ptr};
+
 use minusone::engine::DeobfuscateEngine;
 use widestring::U16String;
 
@@ -17,9 +19,30 @@ fn deobfuscate_powershell(src: String) -> String {
 }
 
 #[no_mangle]
-pub extern "C" fn rust_function(buffer: *const u16, strlen: usize) {
-    unsafe {
-        let name = U16String::from_ptr(buffer, strlen as usize);
-        println!("Hello: {}", name.to_string().unwrap());
-    };
+pub unsafe extern "C" fn rust_function(
+    buffer: *const u16,
+    strlen: c_int,
+    ptr_out: *mut *const u16,
+    outlen: *mut c_int,
+) {
+    let src = U16String::from_ptr(buffer, strlen as usize)
+        .to_string()
+        .unwrap();
+
+    println!("Hello: {}", src);
+
+    let remove_comment = DeobfuscateEngine::remove_extra(&src).unwrap();
+    let mut engine = DeobfuscateEngine::from_powershell(&remove_comment).unwrap();
+    engine.deobfuscate().unwrap();
+
+    let out = engine.lint().unwrap();
+    println!("Bye: {}", out);
+
+    let mut out = U16String::from(out).into_vec();
+    out.shrink_to_fit();
+    let len = out.len();
+
+    *outlen = len as c_int;
+    *ptr_out = out.as_ptr();
+    mem::forget(out);
 }
