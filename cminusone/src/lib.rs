740e4ca874bbc6aec658f81a4061b764b94fa628
atxr <alexandre.tullot@protonmail.com>
2025-03-10 13:55:17 +0100

    Add cminusone lib

---
+++ cminusone/src/lib.rs
@@ -0,0 +1,16 @@
+use minusone::engine::DeobfuscateEngine;
+
+#[cxx::bridge]
+mod ffi {
+    extern "Rust" {
+        fn deobfuscate_powershell(src: String) -> String;
+    }
+}
+
+fn deobfuscate_powershell(src: String) -> String {
+    let remove_comment = DeobfuscateEngine::remove_extra(&src).unwrap();
+    let mut engine = DeobfuscateEngine::from_powershell(&remove_comment).unwrap();
+    engine.deobfuscate().unwrap();
+
+    engine.lint().unwrap()
+}
