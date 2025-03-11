2364d4e56cc12b9bccf4a5c940fda8d3822e99ba
atxr <alexandre.tullot@protonmail.com>
2025-03-11 14:19:43 +0100

    Clean cminusone

--- cminusone/src/lib.rs
+++ cminusone/src/lib.rs
@@ -3,46 +3,26 @@ use std::{ffi::c_int, mem, ptr::null};
 use minusone::engine::DeobfuscateEngine;
 use widestring::U16String;
 
-// #[cxx::bridge(namespace = "cminusone")]
-// mod bridge {
-//     extern "Rust" {
-//         fn deobfuscate_powershell(src: String) -> String;
-//     }
-// }
-
-// fn deobfuscate_powershell(src: String) -> String {
-//     let remove_comment = DeobfuscateEngine::remove_extra(&src).unwrap();
-//     let mut engine = DeobfuscateEngine::from_powershell(&remove_comment).unwrap();
-//     engine.deobfuscate().unwrap();
-
-//     engine.lint().unwrap()
-// }
-
 #[no_mangle]
-pub unsafe extern "C" fn rust_function(buffer: *const u16, strlen: c_int) -> *const u16 {
-    let src = U16String::from_ptr(buffer, strlen as usize)
-        .to_string()
-        .unwrap();
+pub extern "C" fn minusone_deobfuscate_powershell(buffer: *const u16, strlen: c_int) -> *const u16 {
+    unsafe {
+        let src = U16String::from_ptr(buffer, strlen as usize)
+            .to_string()
+            .unwrap();
 
-    println!("Hello: {}", src);
+        if let Ok(remove_comment) = DeobfuscateEngine::remove_extra(&src) {
+            if let Ok(mut engine) = DeobfuscateEngine::from_powershell(&remove_comment) {
+                if engine.deobfuscate().is_ok() {
+                    let out = engine.lint().unwrap();
+                    let mut out = U16String::from(out).into_vec();
+                    out.push(0); // Add null byte
 
-    if let Ok(remove_comment) = DeobfuscateEngine::remove_extra(&src) {
-        println!("Remove extra ok");
-        if let Ok(mut engine) = DeobfuscateEngine::from_powershell(&remove_comment) {
-            println!("from_powershell ok");
-            if engine.deobfuscate().is_ok() {
-                println!("deobfuscate ok");
+                    out.shrink_to_fit();
+                    let ptr = out.as_ptr();
+                    mem::forget(out);
 
-                let out = engine.lint().unwrap();
-                println!("Success: {}", out);
-
-                let mut out = U16String::from(out).into_vec();
-                out.push(0); // Add null byte
-                out.shrink_to_fit();
-                let ptr = out.as_ptr();
-                mem::forget(out);
-
-                return ptr;
+                    return ptr;
+                }
             }
         }
     }
