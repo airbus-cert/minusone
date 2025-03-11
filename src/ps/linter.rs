8f9e2bf3b94bee90a43e5458bfb4917cc032a9ae
atxr <alexandre.tullot@protonmail.com>
2025-03-11 16:58:54 +0100

    Add Debug

--- src/ps/linter.rs
+++ src/ps/linter.rs
@@ -402,7 +402,16 @@ impl<'a> Rule<'a> for RemoveComment {
     /// the down to top
     fn leave(&mut self, node: &Node<'a, Self::Language>) -> MinusOneResult<()> {
         match node.kind() {
-            "program" => self.output += &self.source[self.last_index..],
+            "program" => {
+                println!(
+                    "DEBUGING:\noutput:\n{}\n\nsource:\n{}\n\nlastindex:\n{}\n\nnode:\n{}\n",
+                    self.output,
+                    self.source,
+                    self.last_index,
+                    node.text().unwrap()
+                );
+                self.output += &self.source[self.last_index..]
+            }
             _ => (),
         }
 
