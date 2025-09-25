b632872e09de4be96da2c66c1dbbef74608be6e8
dolphinau <alexandre.tullot@protonmail.com>
2025-09-25 15:46:02 +0200

    use devenv

---
+++ devenv.nix
@@ -0,0 +1,36 @@
+{pkgs, ...}: {
+  packages = [
+    pkgs.maturin
+    pkgs.nil
+    pkgs.nixd
+    pkgs.alejandra
+    pkgs.just
+    pkgs.cargo-component
+    pkgs.just
+    pkgs.gnumake
+  ];
+
+  languages = {
+    python.enable = true;
+    javascript = {
+      enable = true;
+      npm.enable = true;
+    };
+    rust = {
+      channel = "nightly";
+      components = [
+        "cargo"
+        "rust-src"
+        "rustc"
+        "rust-analyzer"
+        "clippy"
+      ];
+      targets = [
+        "wasm32-wasip1"
+        "wasm32-wasip2"
+        "x86_64-unknown-linux-gnu"
+      ];
+      enable = true;
+    };
+  };
+}
