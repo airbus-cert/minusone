9ad02cb7d1febc540c7c152a4cefdcf4c4a7c0bd
dolphinau <alexandre.tullot@protonmail.com>
2025-09-29 15:02:29 +0200

    Add lanaguage selector and errors

--- minusonejs/src/lib.rs
+++ minusonejs/src/lib.rs
@@ -5,16 +5,27 @@ wit_bindgen::generate!({
     world: "minusone",
 });
 
+const LANGUAGES: [&str; 1] = ["Powershell"];
+
 struct Minusone;
 impl Guest for Minusone {
-    fn deobfuscate(source: String) -> String {
-        match DeobfuscateEngine::remove_extra(&source) {
-            Ok(remove_comment) => {
-                let mut engine = DeobfuscateEngine::from_powershell(&remove_comment).unwrap();
-                engine.deobfuscate().unwrap();
-                engine.lint().unwrap()
+    fn get_languages() -> Vec<String> {
+        LANGUAGES.iter().map(|s| s.to_string()).collect()
+    }
+
+    fn deobfuscate(source: String, language: String) -> Result<String, String> {
+        match language.as_str() {
+            "Powershell" => {
+                let without_comments =
+                    DeobfuscateEngine::remove_extra(&source).map_err(|e| format!("{e:?}"))?;
+
+                let mut engine = DeobfuscateEngine::from_powershell(&without_comments)
+                    .map_err(|e| format!("{e:?}"))?;
+                engine.deobfuscate().map_err(|e| format!("{e:?}"))?;
+
+                engine.lint().map_err(|e| format!("{e:?}"))
             }
-            Err(e) => "ERROR: Cannot clean the source\n--> {:?}".to_string(),
+            _ => Err(format!("Error: Language {} not implemented", language)),
         }
     }
 }
