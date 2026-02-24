72c33275fc18f6bbd4d3572d5e88479fc5e6a45a
dolphinau <alexandre.tullot@protonmail.com>
2026-02-24 17:03:31 +0100

    Add with and without bindings to js

--- crates/minusonejs/src/lib.rs
+++ crates/minusonejs/src/lib.rs
@@ -1,5 +1,19 @@
 extern crate minusone;
-use minusone::{engine::DeobfuscateEngine, error::Error};
+use minusone::{engine::DeobfuscateEngine, error::Error as MinusoneError};
+
+enum MinusonejsError {
+    MinusoneError(MinusoneError),
+    JsError(String),
+}
+
+impl ToString for MinusonejsError {
+    fn to_string(&self) -> String {
+        match self {
+            MinusonejsError::JsError(e) => e.clone(),
+            MinusonejsError::MinusoneError(e) => format!("{e:?}"),
+        }
+    }
+}
 
 wit_bindgen::generate!({
     world: "minusone",
@@ -15,10 +29,39 @@ fn return_err(err: String) -> (String, String) {
     (String::from(""), format!("{err:?}"))
 }
 
-fn deobfuscate_powershell(source: String) -> Result<String, Error> {
+fn deobfuscate(
+    source: String,
+    language: String,
+    ruleset: Vec<String>,
+    with: bool,
+) -> Result<String, MinusonejsError> {
+    match language.to_lowercase().as_str() {
+        "powershell" => deobfuscate_powershell(source, ruleset, with)
+            .map_err(|e| MinusonejsError::MinusoneError(e)),
+        _ => Err(MinusonejsError::JsError(format!(
+            "Unsupported language: {}",
+            language,
+        ))),
+    }
+}
+
+fn deobfuscate_powershell(
+    source: String,
+    ruleset: Vec<String>,
+    with: bool,
+) -> Result<String, MinusoneError> {
     let without_comments = DeobfuscateEngine::remove_extra(&source)?;
     let mut engine = DeobfuscateEngine::from_powershell(&without_comments)?;
-    engine.deobfuscate()?;
+
+    match (ruleset.len(), with) {
+        (0, _) => engine.deobfuscate(),
+        (_, true) => {
+            engine.deobfuscate_with_custom_ruleset(ruleset.iter().map(AsRef::as_ref).collect())
+        }
+        (_, false) => {
+            engine.deobfuscate_without_custom_ruleset(ruleset.iter().map(AsRef::as_ref).collect())
+        }
+    }?;
 
     engine.lint()
 }
@@ -30,12 +73,31 @@ impl Guest for Minusone {
     }
 
     fn deobfuscate(source: String, language: String) -> (String, String) {
-        match language.as_str() {
-            "Powershell" => match deobfuscate_powershell(source) {
-                Ok(r) => return_res(r),
-                Err(e) => return_err(format!("{e:?}")),
-            },
-            _ => return_err(format!("Error: Language {} not implemented", language)),
+        match deobfuscate(source, language, vec![], false) {
+            Ok(r) => return_res(r),
+            Err(e) => return_err(e.to_string()),
+        }
+    }
+
+    fn deobfuscate_with(
+        source: String,
+        language: String,
+        ruleset: Vec<String>,
+    ) -> (String, String) {
+        match deobfuscate(source, language, ruleset, true) {
+            Ok(r) => return_res(r),
+            Err(e) => return_err(e.to_string()),
+        }
+    }
+
+    fn deobfuscate_without(
+        source: String,
+        language: String,
+        ruleset: Vec<String>,
+    ) -> (String, String) {
+        match deobfuscate(source, language, ruleset, false) {
+            Ok(r) => return_res(r),
+            Err(e) => return_err(e.to_string()),
         }
     }
 }
