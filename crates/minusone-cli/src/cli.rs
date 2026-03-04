174c4f3afe78dbad01dd534e7f9bd363e5b5ba72
lila <mmimi0593@gmail.com>
2026-03-04 15:56:57 +0100

    Updated clap + moved to a clap struct system

---
+++ crates/minusone-cli/src/cli.rs
@@ -0,0 +1,58 @@
+use clap::{Parser, ValueEnum};
+use std::fmt::Display;
+
+const APPLICATION_NAME: &str = env!("CARGO_PKG_NAME");
+
+#[derive(Debug, Default, Clone, ValueEnum)]
+pub enum Language {
+    #[default]
+    Powershell,
+}
+
+#[derive(Parser, Debug)]
+#[clap(name = APPLICATION_NAME, author, version, about, disable_help_flag = true)]
+pub struct Cli {
+    /// Help information
+    #[arg(long, short)]
+    pub help: bool,
+
+    /// Path to the script file
+    #[arg(long, short)]
+    pub path: String,
+
+    /// Debug mode: print the tree-sitter tree with inferred value on each node
+    #[arg(long, short)]
+    pub debug: bool,
+
+    /// Language of the script (default: powershell)
+    #[arg(long, short)]
+    pub lang: Language,
+
+    /// List rules available for a language
+    #[arg(long, short)]
+    pub list: bool,
+
+    /// Custom comma separated list of rules to apply for the deobfuscation (optional)
+    #[arg(long, short, value_delimiter = ',')]
+    pub rules: Option<Vec<String>>,
+
+    /// Custom comma separated list of rules to skip for the deobfuscation (optional)
+    #[arg(long, short = 'R', value_delimiter = ',')]
+    pub skip_rules: Option<Vec<String>>,
+
+    /// Time computation
+    #[arg(long)]
+    pub time: bool,
+}
+
+impl Display for Language {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Language::Powershell => "powershell".to_string(),
+            }
+        )
+    }
+}
--- crates/minusone-cli/src/main.rs
+++ crates/minusone-cli/src/main.rs
@@ -1,96 +1,22 @@
 extern crate clap;
 extern crate minusone;
+mod cli;
+mod utils;
 
-use clap::{App, Arg};
-use minusone::engine::{DeobfuscateEngine, DeobfuscationBackend};
-use minusone::error::MinusOneResult;
+use clap::Parser;
+use cli::{Cli, Language};
+use minusone::engine::DeobfuscateEngine;
 use minusone::ps::backend::PowershellBackend;
-use std::fmt::Debug;
 use std::{fs, process};
-
-const APPLICATION_NAME: &str = "minusone-cli";
-
-fn run_deobf<B: DeobfuscationBackend>(
-    source: &str,
-    debug: bool,
-    rule_set: Option<Vec<String>>,
-    skip_rule_set: Option<Vec<String>>,
-) -> MinusOneResult<()>
-where
-    <B as DeobfuscationBackend>::Language: Debug,
-{
-    let cleaned = DeobfuscateEngine::<B>::remove_extra(source)?;
-
-    /*let mut engine = DeobfuscateEngine::<B>::from_source(&cleaned)?;
-    engine.deobfuscate()?;*/
-
-    let mut engine = DeobfuscateEngine::<B>::from_source(&cleaned)?;
-
-    if let Some(rules) = rule_set {
-        engine.deobfuscate_with_custom_ruleset(rules.iter().map(AsRef::as_ref).collect())?;
-    } else if let Some(skip_rules) = skip_rule_set {
-        engine
-            .deobfuscate_without_custom_ruleset(skip_rules.iter().map(AsRef::as_ref).collect())?;
-    } else {
-        engine.deobfuscate()?;
-    }
-
-    if debug {
-        engine.debug();
-    } else {
-        println!("{}", engine.lint()?);
-    }
-    Ok(())
-}
+use utils::run_deobf;
 
 fn main() {
-    let matches = App::new(APPLICATION_NAME)
-        .version("0.1.0")
-        .author("Airbus CERT <cert@airbus.com>")
-        .about("A script deobfuscator")
-        .arg(
-            Arg::with_name("path")
-                .long("path")
-                .takes_value(true)
-                .help("Path to the script file"),
-        )
-        .arg(
-            Arg::with_name("lang")
-                .long("lang")
-                .takes_value(true)
-                .help("The language of the script (default: powershell)"),
-        )
-        .arg(
-            Arg::with_name("debug")
-                .long("debug")
-                .help("Print the tree-sitter tree with inferred value on each node"),
-        )
-        .arg(
-            Arg::with_name("list")
-                .long("list")
-                .short("l")
-                .help("List rules available for a language"),
-        )
-        .arg(
-            Arg::with_name("rules")
-                .long("rules")
-                .short("r")
-                .multiple(true)
-                .takes_value(true)
-                .help("Custom comma separated list of rules to apply for the deobfuscation"),
-        )
-        .arg(
-            Arg::with_name("skip-rules")
-                .long("skip-rules")
-                .short("R")
-                .multiple(true)
-                .takes_value(true)
-                .help("Custom comma separated list of rules to skip for the deobfuscation"),
-        )
-        .arg(Arg::with_name("time").long("time").help("Time computation"))
-        .get_matches();
+    let cli = Cli::parse();
+    if cli.help {
+        todo!("Implement clap-help functionality");
+    }
 
-    if matches.is_present("list") {
+    if cli.list {
         println!(
             "Available rules:\n{}",
             DeobfuscateEngine::<PowershellBackend>::language_rules()
@@ -102,41 +28,34 @@ fn main() {
         process::exit(0);
     }
 
-    if matches.is_present("rules") && matches.is_present("skip-rules") {
+    if cli.rules.is_some() && cli.skip_rules.is_some() {
         eprintln!("ERROR: Cannot use --rules and --skip-rules at the same time");
         process::exit(1);
     }
 
-    let source = fs::read_to_string(
-        matches
-            .value_of("path")
-            .expect("Path arguments is mandatory"),
-    )
-    .unwrap();
+    let source = fs::read_to_string(&cli.path).unwrap_or_else(|e| {
+        eprintln!("[x] ERROR: Failed to read file {}: {}", cli.path, e);
+        process::exit(1);
+    });
 
-    let debug = matches.is_present("debug");
+    let debug = cli.debug;
 
-    let rule_set = matches
-        .values_of("rules")
-        .map(|vals| vals.map(str::to_lowercase).collect());
-    let skip_rule_set = matches
-        .values_of("skip-rules")
-        .map(|vals| vals.map(str::to_lowercase).collect());
+    let rule_set = cli
+        .rules
+        .map(|vals| vals.into_iter().map(|s| s.to_lowercase()).collect());
+    let skip_rule_set = cli
+        .skip_rules
+        .map(|vals| vals.into_iter().map(|s| s.to_lowercase()).collect());
 
     let now = std::time::Instant::now();
 
-    let result = match matches.value_of("lang") {
-        Some(lang) => match lang.to_lowercase().as_str() {
-            "powershell" => run_deobf::<PowershellBackend>(&source, debug, rule_set, skip_rule_set),
-            _ => {
-                eprintln!("[x] ERROR: Language {} not implemented", lang);
-                process::exit(1);
-            }
-        },
-        None => run_deobf::<PowershellBackend>(&source, debug, rule_set, skip_rule_set),
+    let result = match cli.lang {
+        Language::Powershell => {
+            run_deobf::<PowershellBackend>(&source, debug, rule_set, skip_rule_set)
+        }
     };
 
-    if matches.is_present("time") {
+    if cli.time {
         let elapsed = now.elapsed();
         println!("\n\nElapsed: {:.2?}", elapsed);
     }
