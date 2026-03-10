67e58a8fb94570672e76ff6beb82e72e000c9683
dolphinau <alexandre.tullot@protonmail.com>
2026-03-10 13:32:55 +0100

    Add transcations to var scope manager

--- core/src/scope.rs
+++ core/src/scope.rs
@@ -20,12 +20,14 @@ impl<T: Clone> Variable<T> {
 #[derive(Clone)]
 pub struct Scope<T: Clone> {
     vars: HashMap<String, Variable<T>>,
+    pending: HashMap<String, Variable<T>>,
 }
 
 impl<T: Clone> Default for Scope<T> {
     fn default() -> Self {
         Self {
             vars: HashMap::new(),
+            pending: HashMap::new(),
         }
     }
 }
@@ -34,46 +36,70 @@ impl<T: Clone> Scope<T> {
     pub fn from(scope: &Scope<T>) -> Self {
         let mut s = Scope {
             vars: scope.vars.clone(),
+            pending: scope.pending.clone(),
         };
-        for (_, var) in s.vars.iter_mut() {
+        for (_, var) in s.vars.iter_mut().chain(s.pending.iter_mut()) {
             var.local = false;
         }
         s
     }
 
-    pub fn assign(&mut self, var_name: &str, value: T) {
-        if let Some(var_value) = self.vars.get_mut(var_name) {
-            var_value.inferred_type = Some(value);
+    pub fn assign(&mut self, var_name: &str, new_value: T, ongoing_transaction: bool) {
+        let var_value = self.vars.get(var_name).cloned();
+
+        let hashmap_to_update = if ongoing_transaction {
+            &mut self.pending
         } else {
-            self.vars
-                .insert(var_name.to_string(), Variable::new(Some(value)));
+            &mut self.vars
+        };
+
+        if let Some(value) = hashmap_to_update.get_mut(var_name) {
+            value.inferred_type = Some(new_value)
+        } else if ongoing_transaction && let Some(mut var_value) = var_value {
+            // If ongoing transaction and value is in self.vars, copy it from there instead of creating a new one
+            var_value.inferred_type = Some(new_value);
+            self.pending.insert(var_name.to_string(), var_value);
+        } else {
+            hashmap_to_update.insert(var_name.to_string(), Variable::new(Some(new_value)));
         }
     }
 
-    pub fn forget(&mut self, var_name: &str) {
-        if let Some(var_value) = self.vars.get_mut(var_name) {
+    pub fn forget(&mut self, var_name: &str, ongoing_transaction: bool) {
+        let hashmap_to_update = if ongoing_transaction {
+            &mut self.pending
+        } else {
+            &mut self.vars
+        };
+
+        if let Some(var_value) = hashmap_to_update.get_mut(var_name) {
             var_value.inferred_type = None;
         }
     }
 
-    pub fn in_use(&mut self, var_name: &str) {
-        if let Some(var_value) = self.vars.get_mut(var_name) {
+    pub fn in_use(&mut self, var_name: &str, ongoing_transaction: bool) {
+        let hashmap_to_update = if ongoing_transaction {
+            &mut self.pending
+        } else {
+            &mut self.vars
+        };
+
+        if let Some(var_value) = hashmap_to_update.get_mut(var_name) {
             var_value.used = true;
         }
     }
 
     pub fn get_var_mut(&mut self, var_name: &str) -> Option<&mut T> {
-        if let Some(data) = self.vars.get_mut(var_name) {
-            return data.inferred_type.as_mut();
-        }
-        None
+        self.pending
+            .get_mut(var_name)
+            .or(self.vars.get_mut(var_name))
+            .and_then(|data| data.inferred_type.as_mut())
     }
 
     pub fn get_var(&self, var_name: &str) -> Option<&T> {
-        if let Some(data) = self.vars.get(var_name) {
-            return data.inferred_type.as_ref();
-        }
-        None
+        self.pending
+            .get(var_name)
+            .or(self.vars.get(var_name))
+            .and_then(|data| data.inferred_type.as_ref())
     }
 
     pub fn get_var_names(&self) -> Vec<String> {
@@ -107,15 +133,35 @@ impl<T: Clone> ScopeManager<T> {
 
     pub fn leave(&mut self) {
         let mut last = self.scopes.pop().unwrap();
-        // we will merge the scope
+
+        // we will merge the pending scope of transcation
+        for (name, value) in last.pending.iter_mut() {
+            if !value.local {
+                if let Some(inferred_type) = &value.inferred_type {
+                    self.current_mut().assign(name, inferred_type.clone(), true);
+                } else {
+                    self.current_mut().forget(name, true);
+                }
+            }
+        }
+
+        // we will merge the vars scope
         for (name, value) in last.vars.iter_mut() {
             if !value.local {
                 if let Some(inferred_type) = &value.inferred_type {
-                    self.current_mut().assign(name, inferred_type.clone());
+                    self.current_mut()
+                        .assign(name, inferred_type.clone(), false);
                 } else {
-                    self.current_mut().forget(name);
+                    self.current_mut().forget(name, false);
                 }
             }
+        }
+    }
+
+    pub fn flush_transaction(&mut self) {
+        let pending = &mut self.current_mut().pending;
+        if !pending.is_empty() {
+            pending.clear();
         }
     }
 
@@ -135,9 +181,9 @@ impl<T: Clone> ScopeManager<T> {
         self.scopes = vec![Scope::default()]
     }
 
-    pub fn forget_everywhere(&mut self, var_name: &str) {
+    pub fn forget_everywhere(&mut self, var_name: &str, ongoing_transaction: bool) {
         for scope in self.scopes.iter_mut() {
-            scope.forget(var_name)
+            scope.forget(var_name, ongoing_transaction)
         }
     }
 }
