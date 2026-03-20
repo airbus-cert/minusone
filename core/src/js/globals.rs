70036288c02b83f9ea912a8a2ae8feb71cd433b0
lila <mmimi0593@gmail.com>
2026-03-20 13:19:01 +0100

    [CORE] Js: added Math as a global object

--- core/src/js/globals.rs
+++ core/src/js/globals.rs
@@ -3,6 +3,7 @@ use crate::js::JavaScript::{NaN, Object, Raw};
 use crate::js::Value::Num;
 use crate::scope::Scope;
 use std::collections::HashMap;
+use std::f64::consts::*;
 
 fn number_obj() -> JavaScript {
     let mut number = HashMap::new();
@@ -20,9 +21,23 @@ fn number_obj() -> JavaScript {
     Object(number)
 }
 
+fn math_obj() -> JavaScript {
+    let mut number = HashMap::new();
+    number.insert("E".to_string(), Raw(Num(E)));
+    number.insert("LN10".to_string(), Raw(Num(LN_10)));
+    number.insert("LN2".to_string(), Raw(Num(LN_2)));
+    number.insert("LOG10E".to_string(), Raw(Num(LOG10_E)));
+    number.insert("LOG2E".to_string(), Raw(Num(LOG2_E)));
+    number.insert("PI".to_string(), Raw(Num(PI)));
+    number.insert("SQRT2".to_string(), Raw(Num(SQRT_2)));
+    number.insert("SQRT1_2".to_string(), Raw(Num(FRAC_1_SQRT_2)));
+    Object(number)
+}
+
 fn js_global_objects() -> HashMap<String, JavaScript> {
     let mut globals = HashMap::new();
     globals.insert("Number".to_string(), number_obj());
+    globals.insert("Math".to_string(), math_obj());
     globals
 }
 
